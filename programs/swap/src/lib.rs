bb8436e5d30579157026f201fd5a6bccd0fd55a2
xiaolin <lixiaolin_23@foxmail.com>
2025-05-22 10:01:27 +0800

    init

---
+++ programs/swap/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("CJuyrPLFyLe73HxcFcXef6cpeBCwPwbdRVCki5eYwdtW");
+
+#[program]
+pub mod swap {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
